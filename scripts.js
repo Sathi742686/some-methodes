b0a2ce7fc562873f560a36b4fab19cb64000c137
Sathi742686 <farjanaahmmed742686@gmail.com>
2023-01-26 01:25:02 +0600

    some methodes completed

---
+++ scripts.js
@@ -0,0 +1,45 @@
+// (1) Array filter()
+// const age1 = [32, 33, 16, 17, 6, 40];
+
+// function isVoter(vAge) {
+//   return vAge >= 18;
+// }
+
+// const voterList = age1.filter(isVoter);
+// console.log(voterList);
+// console.log(age1);
+
+//(2) Array find()
+// const age1 = [32, 33, 16, 17, 6, 40];
+
+// function checkperson(pAge) {
+//   return pAge > 33;
+// }
+
+// const person = age1.find(checkperson);
+// console.log(person);
+
+// // (3) Array map()
+// const num = [32, 33, 16, 17, 6, 40];
+
+// function newFunction(n) {
+//   return n * 2;
+// }
+
+// const newNumber = num.map(newFunction);
+// console.log(newNumber);
+
+// (4) Array reduce()
+// const num = [32, 33, 16, 17, 6, 40];
+
+// function gettotal(first, next) {
+//   return first + next;
+// }
+
+// const total = num.reduce(gettotal);
+// console.log(total);
+
+// Array sort()
+const fruits = ["Banana", "Oringe", "Apple", "Mango"];
+fruits.sort();
+console.log(fruits);
